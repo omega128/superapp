f630a74b7a98c8b3dcd1c80442f95b015a82f76c
KC <omega128@gmail.com>
2017-06-10 21:03:51 -0700

    Re-fix GPS permissions.

--- app/src/main/java/com/example/superapp/superapp/MapsActivity.java
+++ app/src/main/java/com/example/superapp/superapp/MapsActivity.java
@@ -1,6 +1,9 @@
 package com.example.superapp.superapp;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
@@ -13,6 +16,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.content.Context;
 
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -30,6 +36,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import eltos.simpledialogfragment.form.*;
 import okhttp3.*;
 
 import android.util.Log;
@@ -39,6 +46,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private Handler updateMapTimer;
+    private FragmentActivity thisActivity = this;
 
     /* Store Cry Records */
     private Vector<Cry> cryList = new Vector<Cry>();
@@ -59,7 +67,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         public double getLongitude() { return myLongitude; }
     }
 
-
     /* We need to track the user's GPS coordinates */
     private boolean haveGPS = false;
     private String myLocation;
@@ -73,9 +80,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             latitude = location.getLatitude();
             longitude = location.getLongitude();
-
-            myLocation = "Lat: " + latitude + " Long: " + longitude;
-
+            myLocation = "MY LOCATION IS " + "Lat: " + latitude + " Long: " + longitude;
             haveGPS = true;
             Log.d("OHNOES", myLocation);
         }
@@ -119,7 +124,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onResponse(Call call, final Response response) throws java.io.IOException {
                 String responseData = response.body().string();
                 Log.d("OHNOES", "NET CONNECTION SUCCEEDED.");
-                Log.d("OHNOES", "JSON DATA: " + responseData);
 
                 try {
                     JSONObject json = new JSONObject(responseData);
@@ -128,7 +132,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     cryList.clear();
                     for (int i = 0; i < cries.length(); ++i) {
                         JSONObject cry = cries.getJSONObject(i);
-                        Cry newCry = new Cry(cry.getString("c_descr"),
+                        Cry newCry = new Cry(cry.getString("c_title"),
                                 cry.getDouble("c_lat"),
                                 cry.getDouble("c_lon"));
                         cryList.add(newCry);
@@ -149,21 +153,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             for (Cry cry : cryList) {
                 LatLng coords = new LatLng(cry.getLatitude(),
                         cry.getLongitude());
-
-                Log.d("OHNOES", "ADDED MARKER: " + cry.getTitle());
                 mMap.addMarker(new MarkerOptions().position(coords).title(cry.getTitle()));
             }
         }
     }
 
+    private void createCry (String title) {
+        Log.d("OHNOES", "Attempting to create a cry!");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("c_id", "")
+                .addFormDataPart("c_user_id", "1")
+                .addFormDataPart("c_post_date", "2017-06-10")
+                .addFormDataPart("c_title", title)
+                .addFormDataPart("c_descr", "")
+                .addFormDataPart("c_lat", Double.toString(latitude))
+                .addFormDataPart("c_lon", Double.toString(longitude))
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://kcchambers.dreamhosters.com/scc/superapp/create.php")
+                .method("POST", RequestBody.create(null, new byte[0]))
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("OHNOES", "NET CONNECTION FAILED.");
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws java.io.IOException {
+                Log.d("OHNOES", "CRY SENT!!!");
+            }
+        });
+    }
 
     @Override
     public void onRequestPermissionsResult (int requestCode,
                                      String[] permissions,
                                      int[] grantResults)
-
     {
-        Log.d("OHNOES", "Grant results: " + grantResults[0]);
+        //Log.d("OHNOES", "Grant results: " + grantResults[0]);
+    }
+
+    public void showCreateCryDialog () {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Create Cry For Help");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                createCry (input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 
 
@@ -181,13 +240,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         updateMapTimer = new Handler(getApplicationContext().getMainLooper());
         updateMapTimer.postDelayed(updateMapTimerThread, 1);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
         // check if the user has given permission to use their GPS coords.
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.d("OHNOES", "USER HAS FORBIDDEN ACCESS TO GPS!");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
-        else
-        {
+
+        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
             //set up a location listener
             LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             MyCurrentLocationListener locationListener = new MyCurrentLocationListener();
@@ -206,17 +267,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         /* the user creates a new cry for help by long clicking the map */
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-
             @Override
             public void onMapLongClick(LatLng latLng) {
-                if (haveGPS) {
-                    // todo: bring up New Cry dialog
-                }
-                else
-                {
-                    // todo: complain if user has no gps
-
-                }
+                showCreateCryDialog();
             }
         });
 
